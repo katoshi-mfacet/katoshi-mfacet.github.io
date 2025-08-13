3eef2fd57af6dfa0f19feaeda2fa64c1933a98bc
katoshi-mfacet <katoshi.co@gmail.com>
2025-08-13 02:18:53 +0000

    Deploy: 2025-08-13 02:18:53

---
+++ _next/static/9iPl-YMh58KPqI8k1Zle0/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[lang]","\u002F[lang]\u002Farchive","\u002F[lang]\u002Farchive\u002F[year]\u002F[month]","\u002F[lang]\u002Farticles","\u002F[lang]\u002Farticles\u002F[slug]","\u002F[lang]\u002Fcategories\u002F[category]","\u002F[lang]\u002Ftags\u002F[tag]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
